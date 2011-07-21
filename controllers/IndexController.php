dc49ebc9c86ef6261255016002b3f40bedc680fc
Julien Sicot <julien.sicot@gmail.com>
2011-07-21 11:48:00 +0200

    remove autocompetion to the DC Spatial Coverage element

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -68,34 +68,4 @@ class Idref_IndexController extends Omeka_Controller_Action
         $this->_helper->json($subjects);
     }
 
-	public function geoProxyAction()
-    {
-
-		$config = array(
-		'adapter' => 'Zend_Http_Client_Adapter_Proxy',
-		'proxy_host' => IDREF_PROXY_HOST,
-		'proxy_port' => IDREF_PROXY_PORT
-
-		);
-        $client = new Zend_Http_Client();
-        $client->setUri(self::IDREF_SUGGEST_URL);
-		$client->setConfig($config);
- 		$request = $this->getRequest()->getParam('term');
-		$request = strtolower($request);
-		$request = regAccents($request);
-		$trimmed = trim($request); 
-		$trimmed_array = explode(" ",$trimmed);	
-		$request = implode(" AND ", $trimmed_array);
-		$request ="(" . $request . "*)";		
-		$request = "geogname_t:".$request;
-       	$client->setParameterGet('q',$request);
-        $json = json_decode($client->request()->getBody(),true);
-		$geo=array();
-		foreach($json[response][docs] as $d)
-		{
-		array_push($geo, $d[affcourt_z]);
-		}
-		
-        $this->_helper->json($geo);
-    }
 }
